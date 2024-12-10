c978fc9577082743d65adc4a9bf9c21d10c327f8
Jon Tan <jonathanytan@gmail.com>
2024-12-10 16:36:40 -0500

    d3p1 completed

---
+++ Day 3/day3_part1.cpp
@@ -0,0 +1,38 @@
+#include <fstream>
+#include <iostream>
+#include <regex>
+
+int main() {
+	std::string str;
+	std::string temp_str;
+
+	std::ifstream myFile("input.txt");
+
+	if(myFile.is_open()) {
+		while(getline(myFile, temp_str)) {
+			str += temp_str;
+		}
+	}
+
+	myFile.close();
+
+	std::smatch str_match;
+	std::regex reg("mul\\((\\d+),(\\d+)\\)");
+	
+	std::sregex_iterator currentMatch(str.begin(), str.end(), reg);
+	std::sregex_iterator lastMatch;
+
+	int sum = 0;
+
+	while(currentMatch != lastMatch) {
+		std::smatch match = *currentMatch;
+
+		//.str(x) returns string of the xth captured group defined in the regex
+		sum += stoi(match.str(1)) * stoi(match.str(2));
+		currentMatch++;
+	}
+
+	std::cout << sum << std::endl;
+
+	return 0;
+}
--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 Used unordered map in part 2 to tally frequencies of right column (2n vs. n^2 run time).
 
 ## Day 2
-Brushed up on reading input from a file. .erase() member function completely slipped my mind for part 2. Is there a more efficient way? Brute force was simple after that.
+Brushed up on reading input from a file. `.erase()` member function completely slipped my mind for part 2; need a refresher on STL perhaps. Is there a more efficient way? Brute force was simple after that.
+
+## Day 3
+Regex resources consulted: 
+
+https://regexone.com/lesson/matching_characters?
+https://regexr.com/
+https://youtu.be/9K4N6MO_R1Y?si=0tsnSmABvHDySL8a
+https://stackoverflow.com/questions/42983337/how-to-get-only-given-captured-group-regex-c
